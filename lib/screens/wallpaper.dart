bf212be44beeb8114668b903d7028ed43feaf8a6
khalidabdou <37934938+khalidabdou@users.noreply.github.com>
2020-02-05 22:14:46 +0100

    update 2

--- lib/screens/wallpaper.dart
+++ lib/screens/wallpaper.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
@@ -5,12 +7,14 @@ import 'package:flutter_app/data/dbManager.dart';
 import 'package:flutter_app/models/image.dart';
 import 'package:flutter_app/models/itemImage.dart';
 import 'package:flutter_app/util/constant.dart';
-import 'package:image_picker_saver/image_picker_saver.dart';
+import 'package:flutter_app/util/util.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
-import 'package:toast/toast.dart';
+import 'package:path/path.dart' as path;
+import 'dart:io';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter/services.dart';
 
 class WallpaperPage extends StatefulWidget {
   static final String id = "wallpaper";
@@ -132,7 +136,7 @@ class _WallpaperPageState extends State<WallpaperPage> {
   }
 }
 
-class utilBar extends StatelessWidget {
+class utilBar extends StatefulWidget {
   const utilBar({
     Key key,
     @required this.widget,
@@ -142,7 +146,128 @@ class utilBar extends StatelessWidget {
   final WallpaperPage widget;
   final itemImage currentImage;
 
-  _savePhoto(context) {}
+  @override
+  _utilBarState createState() => _utilBarState();
+}
+
+class _utilBarState extends State<utilBar> {
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  bool isLoading;
+
+  _showSnackBar(String text, {Duration duration = const Duration(seconds: 1)}) {
+    return scaffoldKey.currentState
+        ?.showSnackBar(SnackBar(content: Text(text), duration: duration));
+  }
+
+  void _done() {
+    if (mounted) {
+      setState(() => isLoading = false);
+    }
+  }
+
+  Future _downloadImage() async {
+    try {
+      setState(() => isLoading = true);
+
+      final targetPlatform = Theme.of(context).platform;
+
+      if (targetPlatform == TargetPlatform.android) {
+        // request runtime permission
+        final permissionHandler = PermissionHandler();
+        final status = await permissionHandler
+            .checkPermissionStatus(PermissionGroup.storage);
+        if (status != PermissionStatus.granted) {
+          final requestRes = await permissionHandler
+              .requestPermissions([PermissionGroup.storage]);
+          if (requestRes[PermissionGroup.storage] != PermissionStatus.granted) {
+            _showSnackBar('Permission denined. Go to setting to granted!');
+            return _done();
+          }
+        }
+      }
+
+      // get external directory
+      Directory externalDir;
+      switch (targetPlatform) {
+        case TargetPlatform.android:
+          externalDir = await getExternalStorageDirectory();
+          break;
+        case TargetPlatform.fuchsia:
+          _showSnackBar('Not support fuchsia');
+          return _done();
+        case TargetPlatform.iOS:
+          externalDir = await getApplicationDocumentsDirectory();
+          break;
+        case TargetPlatform.macOS:
+          // TODO: Handle this case.
+          break;
+      }
+      print('externalDir=$externalDir');
+
+      final filePath = path.join(
+          externalDir.path, 'flutterImages', widget.currentImage.urlImage);
+
+      final file = File(filePath);
+      if (file.existsSync()) {
+        file.deleteSync();
+      }
+
+      print('Start download...');
+      final bytes = await http.readBytes(
+          constant.SERVER_IMAGE_UPFOLDER_CATEGORY +
+              'Good%20Evening/' +
+              widget.currentImage.urlImage);
+      print(widget.currentImage.urlImage);
+      print('Done download...');
+
+      final queryData = MediaQuery.of(context);
+      final width =
+          (queryData.size.shortestSide * queryData.devicePixelRatio).toInt();
+      final height =
+          (queryData.size.longestSide * queryData.devicePixelRatio).toInt();
+
+      final Uint8List outBytes = await methodChannel.invokeMethod(
+        resizeImage,
+        <String, dynamic>{
+          'bytes': bytes,
+          'width': width,
+          'height': height,
+        },
+      );
+
+      //save image to storage
+      final saveFileResult =
+          saveImage({'filePath': filePath, 'bytes': outBytes});
+
+      _showSnackBar(
+        saveFileResult
+            ? 'Image downloaded successfully'
+            : 'Failed to download image',
+      );
+
+      // call scanFile method, to show image in gallery
+      /* unawaited(
+        methodChannel
+            .invokeMethod(
+              scanFile,
+              <String>['flutterImages', '${imageModel.id}.png'],
+            )
+            .then((result) => print('Scan file: $result'))
+            .catchError((e) => print('Scan file error: $e')),
+      );*/
+
+      // increase download count
+      /*  _increaseCount('downloadCount', imageModel.id);*/
+    } on PlatformException catch (e) {
+      _showSnackBar(e.message);
+    } catch (e, s) {
+      _showSnackBar('An error occurred');
+      debugPrint('Download image: $e, $s');
+    }
+
+    return _done();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -163,38 +288,27 @@ class utilBar extends StatelessWidget {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: <Widget>[
-                    currentImage.isfav == 1
+                    widget.currentImage.isfav == 1
                         ? IconButton(
                             icon: Icon(Icons.favorite),
                             color: Colors.red,
                             onPressed: () {
-                              currentImage.isfav = 0;
+                              widget.currentImage.isfav = 0;
                               Provider.of<DataProvider>(context, listen: false)
-                                  .deletefav(currentImage);
+                                  .deletefav(widget.currentImage);
                             },
                           )
                         : IconButton(
                             icon: Icon(Icons.favorite_border),
                             onPressed: () {
-                              currentImage.isfav = 1;
+                              widget.currentImage.isfav = 1;
                               Provider.of<DataProvider>(context, listen: false)
-                                  .addToFav(currentImage);
+                                  .addToFav(widget.currentImage);
                             },
                           ),
                     IconButton(
                       icon: Icon(Icons.file_download),
-                      onPressed: () {
-                        if (PermissionHandler().checkPermissionStatus(
-                                PermissionGroup.storage) !=
-                            PermissionStatus.granted)
-                          PermissionHandler().requestPermissions([
-                            PermissionGroup.storage
-                          ]).then(_savePhoto(context));
-                        // setState(() {
-                        //   downloading = true;
-                        // });
-                        // downloadImage();
-                      },
+                      onPressed: _downloadImage,
                     ),
                     // Text(
                     //   !downloading
@@ -223,11 +337,5 @@ class utilBar extends StatelessWidget {
         )
       ],
     );
-  }
-
-  void _onload(bool t) {
-    if (t) {
-      showDialg(context: context);
-    }
   }
 }
--- lib/util/constant.dart
+++ lib/util/constant.dart
@@ -1,21 +1,69 @@
+import 'package:flutter/services.dart';
+
+const String channel = 'my_flutter_wallpaper';
+const methodChannel = MethodChannel(channel);
+
+/// Set image as wallpaper
+/// Arguments: [List] of [String]s, is path of image file, start from folder in external storage directory
+/// Return   : a [String] when success or [PlatformException] when failed
+/// Example:
+/// path of image: 'external storage directory'/flutterImages/image.png
+///   methodChannel.invokeMethod(
+///      setWallpaper,
+///      <String>['flutterImages', 'image.png'],
+///   );
+const String setWallpaper = 'setWallpaper';
+
+/// Scan image file, after scan, we can see image in gallery
+/// Arguments: [List] of [String]s, is path of image file, start from folder in external storage directory
+/// Return   : a [String] when success or [PlatformException] when failed
+/// Example:
+/// path of image: 'external storage directory'/flutterImages/image.png
+///   methodChannel.invokeMethod(scanFile, <String>[
+///     'flutterImages',
+///     'image.png'
+///   ]);
+const String scanFile = 'scanFile';
+
+/// Share image to facebook
+/// Arguments: [String], it is image url
+/// Return   : [Null]
+/// Example:
+/// methodChannel.invokeMethod(shareImageToFacebook, url);
+const String shareImageToFacebook = 'shareImageToFacebook';
+
+/// Resize image
+/// Arguments: [Map], keys is [String]s, values is dynamic type
+/// Return   : a [Uint8List] when success or [PlatformException] when failed
+/// Example:
+/// final Uint8List outBytes = await methodChannel.invokeMethod(
+///   resizeImage,
+///   <String, dynamic>{
+///     'bytes': bytes,
+///     'width': 720,
+///     'height': 1280,
+///   },
+/// );
+const String resizeImage = 'resizeImage';
+
 class constant {
   //this is the path of uploaded image of server where image store
   static final String SERVER_IMAGE_UPFOLDER_CATEGORY =
-      "http://www.dev3pro.com/WallpaperApp00/categories/";
+      "http://dev3pro.com/WallpaperApp00/categories/";
 
   //this is the path of uploaded image of server where image store
   static final String SERVER_IMAGE_UPFOLDER_THUMB =
-      "http://www.dev3pro.com/WallpaperApp00/images/thumbs/";
+      "http://dev3pro.com/WallpaperApp00/images/thumbs/";
 
   //this url is used to get latest 15 image in 1st tab.here 15 indicate that display latest 15 image if you want change to another then do.
   static final String LATEST_URL =
-      "http://www.dev3pro.com/WallpaperApp00/api.php?latest=100";
+      "http://dev3pro.com/WallpaperApp00/api.php?latest=20";
   //this url gives list of category in 2nd tab
   static final String CATEGORY_URL =
-      "http://www.dev3pro.com/WallpaperApp00/api.php";
+      "http://dev3pro.com/WallpaperApp00/api.php";
   //this url gives item of specific category.
   static final String CATEGORY_ITEM_URL =
-      "http://www.dev3pro.com/WallpaperApp00/api.php?cat_id=";
+      "http://dev3pro.com/WallpaperApp00/api.php?cat_id=";
 
   static final String LATEST_ARRAY_NAME = "HDwallpaper";
   static final String LATEST_IMAGE_CATEGORY_NAME = "category_name";
